b0451fb56d598484367a0e5d62d58ce29255f6c1
RavishAhmad <114918290+RavishAhmad@users.noreply.github.com>
2023-09-14 00:37:51 +0530

    Add files via upload

--- bin/activate.fish
+++ bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/amjadnagori/PycharmProjects/python_udemy/webserver"
+set -gx VIRTUAL_ENV "/Users/amjadnagori/PycharmProjects/python_udemy/webserver/webserver"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
